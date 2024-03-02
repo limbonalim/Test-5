8555663e767479fe513a4897b966d7f01d2bf878
Zhuravlev Petr <zeterfrost@gmail.com>
2024-03-02 17:40:51 +0600

    added fixtures for API

--- api/fixtures.ts
+++ api/fixtures.ts
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose';
 import config from './config';
-import Artist from './models/artistsSchema';
-import Album from './models/albumsSchema';
-import Track from './models/tracksSchema';
+import User from './models/usersSchema';
+import Product, { Category } from './models/productsSchema';
 
 const dropCollection = async (
 	db: mongoose.Connection,
@@ -19,12 +18,70 @@ const run = async () => {
 	await mongoose.connect(config.mongoose);
 	const db = mongoose.connection;
 
-	const collections = [];
+	const models = [User, Product];
 
-	for (const collectionName of collections) {
-		await dropCollection(db, collectionName);
+	for (const model of models) {
+		await dropCollection(db, model.collection.collectionName);
 	}
 
+	const [userOne, userTwo, userThree] = await User.create(
+		{
+			username: 'admin',
+			password: '123321',
+			displayName: 'AdminOne',
+			phone: '+996704444444',
+			token: crypto.randomUUID(),
+		},
+		{
+			username: 'adminTwo',
+			password: '123321',
+			displayName: 'Super Admin',
+			phone: '+996704321444',
+			token: crypto.randomUUID(),
+		},
+		{
+			username: 'user',
+			password: '123321',
+			displayName: 'User',
+			phone: '+996704333333',
+			token: crypto.randomUUID(),
+		},
+	);
+
+	await Product.create(
+		{
+			title: 'Honda Accord',
+			description: 'Honda Accord - 2004 год, машина живая и т д',
+			price: 3000,
+			user: userOne,
+			category: Category.cars,
+			image: '/fixtures/accord.jpeg',
+		},
+		{
+			title: 'Продам ноутбук',
+			description: 'Продам ноутбук',
+			price: 220,
+			user: userTwo,
+			category: Category.computers,
+			image: '/fixtures/computer.webp',
+		},
+		{
+			title: 'Продам стол недорого',
+			description: 'Продам офисный стол недорого',
+			price: 320,
+			user: userThree,
+			category: Category.tables,
+			image: '/fixtures/table.jpeg',
+		},
+		{
+			title: 'Iphone 12',
+			description: 'Продам IPhone 12 pro',
+			price: 400,
+			user: userOne,
+			category: Category.other,
+			image: '/fixtures/Iphone.jpeg',
+		},
+	);
 	await db.close();
 };
 
